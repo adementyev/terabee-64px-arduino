b862127ce2c3e63056080fbb3a22bdf2f98486d5
Artem Dementyev <artem.dementyev@gmail.com>
2019-07-23 13:41:04 -0400

    Update terra_bee_sample_uart_code_arduino.ino

--- terra_bee_sample_uart_code_arduino.ino
+++ terra_bee_sample_uart_code_arduino.ino
@@ -14,9 +14,8 @@ uint8_t prev_inChar;
  
 
 void setup() {
-  pinMode(13, OUTPUT);              // Initialize digital pin 13 as an output (ERASABLE if the communication works)
-  Serial.begin(0);             // Open serial port 0 (which corresponds to USB port and pins TX0 and RX0), set data rate to 115200 bps
-  Serial1.begin(3000000);            // Open serial port 1 (which corresponds to pins TX1 and RX1), set data rate to 115200 bps
+  Serial.begin(0);             // Open serial port for USB serrial
+  Serial1.begin(3000000);            // Open serial port 1
 }
 
 
